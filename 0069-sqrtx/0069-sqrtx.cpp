a8d6e16c66a1a8fd0a2052e62fd8c6f9676b9302
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-07-09 21:07:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x==0||x==1){
+            return x;
+        }
+        int l=0;
+        int r=x/2;
+        int ans=1;
+        while (l<=r){
+            int mid=l+(r-l)/2;
+            long long sq=1LL*mid*mid;
+            if (sq==x){
+                return mid;
+            }
+            else if (sq<x){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
