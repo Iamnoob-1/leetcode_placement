a5b5cb389214aebc210ecb8c4a1cdd480849b275
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-07-23 10:44:50 +0530

    Time: 0 ms (100%), Space: 34.4 MB (16.06%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findmin(TreeNode*root){
+        while (root->left){
+            root=root->left;
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root)return NULL;
+        if (root->val>key){
+            root->left=deleteNode(root->left,key);
+        }
+        else if (root->val<key){
+            root->right=deleteNode(root->right,key);
+        }
+        else {
+            //check for how many kids he has 
+            //0 child 
+            if (!root->left&&!root->right){
+                delete(root);
+                return NULL;
+            }
+            //1 child
+            else if ((!root->left&&root->right)||(root->left&&!root->right)){
+                TreeNode*temp=root->left==NULL?root->right:root->left;
+                delete(root);
+                return temp;
+            }
+            //2child
+            //find smallest element in right subtree of the deleeting node and replace it with it 
+            else{
+                TreeNode* small=findmin(root->right);
+                root->val = small->val;
+                root->right = deleteNode(root->right,small->val);
+            }
+        }
+        return root;
+    }
+};
