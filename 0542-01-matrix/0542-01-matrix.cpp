885bb31454e09e91536fa4a5d6d8658e42f1e4ad
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-07-26 02:03:06 +0530

    Time: 3 ms (98.63%), Space: 34.7 MB (87.08%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,7 +5,6 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        // vector<vector<int>>dist
         queue<pair<int,int>>q;
         for (int i=0;i<n;i++){
             for (int j=0;j<m;j++){
