e0ace41a8a9ab27c4aa09536988314f65557ede0
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-08-01 00:02:19 +0530

    Time: 2 ms (15.78%), Space: 8.6 MB (27.42%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n=word.length();
-        int blocks=n/8;
-        return (blocks * (blocks + 1) * 4)+ (n % 8) * (blocks + 1);
+        vector<int>freq(26,0);
+        for (char ch:word){
+            freq[ch-'a']++;
+        }
+        sort(freq.begin(), freq.end(), greater<int>());
+        int ans=0;
+        for (int i=0;i<26;i++){
+            ans+=freq[i]*(i/8+1);
+        }
+        return ans;
     }
 };
