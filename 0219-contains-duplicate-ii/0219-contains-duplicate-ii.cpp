3e640062c7294b4ada7c63e81e0fa82b246792bb
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-08-08 21:44:47 +0530

    Time: 119 ms (14.83%), Space: 116.8 MB (36.55%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_set<int>mp;
+
+        for (int i=0;i<n;i++){
+            if(mp.count(nums[i]))return true;
+            mp.insert(nums[i]);
+            if(i>=k)mp.erase(nums[i-k]);
+        }
+        return false;
+    }
+};
