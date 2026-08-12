287110ba207d1392203252613bedc9ac8dfdf339
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-08-12 21:53:16 +0530

    Time: 27 ms (75.61%), Space: 8.9 MB (76.75%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool recursion(int n,vector<int>&dp){
-        if (n==0)return false;
-        if (n==1)return true;
-        if (dp[n]!=-1)return dp[n];
-        for (int i=1;i*i<=n;i++){
-             if(recursion(n -i*i,dp)==false)
-            return dp[n] = true;
-        }
-        return dp[n]=false;
-    }
     bool winnerSquareGame(int n) {
-        vector<int>dp(n+1,-1);
-        return recursion(n,dp);
-        
+        vector<bool> dp(n + 1, false);
+        dp[0] = false;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                int square = j * j;
+                if (!dp[i - square]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
     }
 };
