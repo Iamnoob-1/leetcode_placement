40c0f941b3b0cb66f55ea67d6641e261b6b6f043
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-08-14 00:46:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int recursion(int n,vector<int>&dp){
-        if (n==0)return 1;
-        if (n<=3)return n;
+        if (n==0)return 0;
+        if (n==1)return 1;
+        if (n==2)return 2;
         if (dp[n]!=-1)return dp[n];
-        return dp[n]=recursion(n-1,dp)+recursion(n-2,dp);
+        int onestep=recursion(n-1,dp);
+        int twostep=recursion(n-2,dp);
+        return dp[n]=onestep+twostep;
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
