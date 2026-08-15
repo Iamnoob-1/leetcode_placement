b55058afbfb5029bb776e175862c883cad5981c9
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-08-15 16:11:38 +0530

    Time: 170 ms (23.2%), Space: 204.2 MB (7.8%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    bool recursion(int index,int target,vector<int>&nums,int sum,vector<vector<int>>&dp){
-        if (sum==target)return true;
-        if (index>=nums.size())return false;
-        if (dp[index][sum]!=-1)return dp[index][sum];
+    bool recursion(int index,int remtarget,vector<int>&nums,vector<vector<int>>&dp){
+        if (remtarget==0)return true;
+        if (remtarget<0)return false;
+        if (index<0)return false;
+        if (dp[index][remtarget]!=-1)return dp[index][remtarget];
 
-        bool take = recursion(index+1,target,nums,sum+nums[index],dp);
-        bool nottake = recursion(index+1,target,nums,sum,dp);
-        return dp[index][sum]=take|| nottake;
+        bool take = recursion(index-1,remtarget-nums[index],nums,dp);
+        bool nottake = recursion(index-1,remtarget,nums,dp);
+        return dp[index][remtarget]=take|| nottake;
     }
     bool canPartition(vector<int>& nums) {
         int target=0;
         for (int num:nums){
             target+=num;
         }
+        int n=nums.size();
         if (target%2!=0)return false;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
-        return recursion(0,target/2,nums,0,dp);
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return recursion(n-1,target/2,nums,dp);
     }
 };
