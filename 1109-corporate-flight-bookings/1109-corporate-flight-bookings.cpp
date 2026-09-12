20fcd21b199952de1d3d8a1b1f94dc4eb2f61a4e
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-09-12 19:07:33 +0530

    Time: 2136 ms (8.88%), Space: 78.6 MB (21.67%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int>ans(n,0);
+        vector<pair<int,int>>seat;
+        for (auto it:bookings){
+            int f=it[0];
+            int s=it[1];
+            int r=it[2];
+            for (int i=f;i<=s;i++){
+                ans[i-1]+=r;
+            }
+        }
+        return ans;
+
+
+    }
+};
