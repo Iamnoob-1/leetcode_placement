2833e46d64e4db8ab0d4478ea6e1e9db677445d2
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-09-15 01:30:30 +0530

    Time: 600 ms (8.41%), Space: 296.7 MB (6.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int recursion(int idx,vector<int>&nums,vector<vector<int>>&dp,int prev){
-        if(idx>=nums.size())return 0;
+        if (idx>=nums.size())return 0;
+        int take=INT_MIN;
         if (dp[idx][prev+1]!=-1)return dp[idx][prev+1];
-        int take=-1;
-        if (prev==-1||nums[idx]>nums[prev])
+        if (prev==-1 || nums[prev]<nums[idx]){
             take=1+recursion(idx+1,nums,dp,idx);
+        }
         int nottake=recursion(idx+1,nums,dp,prev);
         return dp[idx][prev+1]=max(take,nottake);
     }
