365a3f2b712d8c370deac758fe2bdb2da5682b53
Rishmeet Singh <rishmeetsingh2005@gmail.com>
2026-09-27 00:30:00 +0530

    Time: 11 ms (65.51%), Space: 31.8 MB (40.58%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage,
-                                int k) {
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage,int k) {
         vector<pair<double,double>> rate;
         for (int i = 0; i < quality.size(); i++) {
             rate.push_back({(1.0 * wage[i]) / (1.0 * quality[i]), i});
@@ -11,18 +10,16 @@ public:
         double sum = 0;
         double mini = DBL_MAX;
         for (int i = 0; i < rate.size(); i++) {
-            sum += quality[rate[i].second];
+            sum+=quality[rate[i].second];
             pq.push(quality[rate[i].second]);
-            if (pq.size() < k)
-                continue;
-            while (pq.size() > k) {
-                int tp = pq.top();
-                sum -= tp;
+            if (pq.size()>k){
+                int temp=pq.top();
+                sum-=temp;
                 pq.pop();
             }
             if (pq.size()==k){
-                double ans = sum * rate[i].first;
-                mini = min(mini, ans);
+                double ans=sum*rate[i].first;
+                mini=min(ans,mini);
             }
         }
         return mini;
